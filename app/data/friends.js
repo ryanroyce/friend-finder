5fca2f4ee817825b53d6197c7cb03b538dade3c5
dissonance <ryanminor01u@yahoo.com>
2019-01-19 13:37:15 -0600

    changed friends array

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 let friendsArray = [
     {
-        name: "Jimmy Dwyer",
-        picture: "https://vignette.wikia.nocookie.net/vsbattles/images/7/7e/Earthworm_jim_by_zeefster-d46dp3z.png/revision/latest?cb=20170102021211",
+        name: "Jules Winnfield",
+        picture: "https://m.media-amazon.com/images/M/MV5BMWIwOTZmMjYtNzE4Ny00MjM5LWI3OTYtMWVhM2EwYjNiYjQzXkEyXkFqcGdeQXVyNjUwNzk3NDc@._V1_.jpghttps://vignette.wikia.nocookie.net/vsbattles/images/7/7e/Earthworm_jim_by_zeefster-d46dp3z.png/revision/latest?cb=20170102021211",
         scores: [
             "1",
             "1",
@@ -16,24 +16,24 @@ let friendsArray = [
         ]
     },
     {
-        name: "The Z Show",
-        picture: "https://media.npr.org/assets/artslife/arts/2010/09/galifianakis-aa06dc89e0b14a5f9c88556fc85b6dc997814c19-s800-c85.jpg",
+        name: "Vincent Vega",
+        picture: "https://m.media-amazon.com/images/M/MV5BMWZkOTFmMTgtYTU1Zi00MzE4LTgyNTctZTYxODUyMjg2MThhXkEyXkFqcGdeQXVyNDIyNjA2MTk@._V1_SY1000_CR0,0,1517,1000_AL_.jpg",
         scores: [
-            "5",
-            "5",
-            "5",
-            "5",
-            "5",
-            "5",
-            "5",
-            "5",
-            "5",
-            "5"
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2"
         ]
     },
     {
-        name: "Erlich Bachman",
-        picture: "http://s3.amazonaws.com/assets.piedpiper.pro/app/uploads/2016/06/piedpiper_308_blog_01.jpg",
+        name: "Butch Coolidge",
+        picture: "https://m.media-amazon.com/images/M/MV5BMTAxMzE0NTE1OTVeQTJeQWpwZ15BbWU4MDk5OTI4OTEx._V1_SX1486_CR0,0,1486,999_AL_.jpgttp://s3.amazonaws.com/assets.piedpiper.pro/app/uploads/2016/06/piedpiper_308_blog_01.jpg",
         scores: [
             "3",
             "3",
@@ -45,6 +45,38 @@ let friendsArray = [
             "3",
             "3",
             "3"
+        ]
+    },
+    {
+        name: "Marsellus Wallace",
+        picture: "https://m.media-amazon.com/images/M/MV5BNzAzMGYwZDEtNzJhYS00MDRmLTlhMGMtY2QzZDJmOTJmNjAwL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNDAxOTExNTM@._V1_.jpg",
+        scores: [
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4"
+        ]
+    },
+    {
+        name: "Mia Wallace",
+        picture: "https://m.media-amazon.com/images/M/MV5BMTQ2MDk3ODAwMV5BMl5BanBnXkFtZTgwNzE4OTEwMjE@._V1_SY1000_CR0,0,1463,1000_AL_.jpg",
+        scores: [
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5"
         ]
     }
 ];
